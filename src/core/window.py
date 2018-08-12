8d56aff927bf2961c082e9a7961810ba68570acc
Bukhari M <bukharim96@gmail.com>
2018-08-12 18:00:03 +0200

    Extend Window API

--- src/core/window.py
+++ src/core/window.py
@@ -5,45 +5,43 @@ import webview
 
 class Window:
   def __init__(self):
-    self.window_title = "New Window"
     self.window_uid = None
   
   def create(self, config):
     if self.window_uid:
       return
-    # title=config["title"] if config["title"] else "New Window",
-    # url=config["url"] if config["url"] else "",
-    # js_api=config["nativeAPI"] if config["nativeAPI"] else None,
-    # width=config["width"] if config["width"] else 800,
-    # height=config["height"] if config["height"] else 600,
-    # resizable=config["resizable"] if config["resizable"] else True,
-    # fullscreen=config["fullscreen"] if config["fullscreen"] else False,
-    # min_size=(
-    #   config["minWidth"] if config["minWidth"] else 200,
-    #   config["minHeight"] if config["minHeight"] else 100
-    # ),
-    # strings=config["strings"] if config["strings"] else {},
-    # confirm_quit=config["confirmQuit"] if config["confirmQuit"] else False,
-    # background_color=config["backgroundColor"] if config["backgroundColor"] else '#FFF',
-    # debug=config["debug"] if config["debug"] else False,
-    # text_select=config["userSelect"] if config["userSelect"] else False
 
-    self.window_title = ''.join(config["title"])
+    title=''.join(config["title"]) if "title" in config else "New Window"
+    url=''.join(config["url"]) if "url" in config else ""
+    # js_api=config["nativeAPI"] if "nativeAPI" in config else None
+    width=config["width"] if "width" in config else 800
+    height=config["height"] if "height" in config else 600
+    resizable=config["resizable"] if "resizable" in config else True
+    fullscreen=config["fullscreen"] if "fullscreen" in config else False
+    min_size=(
+      config["minWidth"] if "minWidth" in config else 200,
+      config["minHeight"] if "minHeight" in config else 100
+    )
+    strings=config["strings"] if "strings" in config else {}
+    confirm_quit=config["confirmQuit"] if "confirmQuit" in config else False
+    background_color=''.join(config["backgroundColor"]) if "backgroundColor" in config else '#FFF'
+    debug=config["debug"] if "debug" in config else False
+    text_select=config["userSelect"] if "userSelect" in config else False
 
     self.window_uid = webview.create_window(
-      title=''.join(config["title"]),
-      url=''.join(config["url"]),
-      # js_api=''.join(config["js_api"]),
-      # width=''.join(config["width"]),
-      # height=''.join(config["height"]),
-      # resizable=''.join(config["resizable"]),
-      # fullscreen=''.join(config["fullscreen"]),
-      # min_size=''.join(config["min_size"]),
-      # strings=''.join(config["strings"]),
-      # confirm_quit=''.join(config["confirm_quit"]),
-      # background_color=''.join(config["background_color"]),
-      # debug=''.join(config["debug"]),
-      # text_select=''.join(config["text_select"])
+      title=title,
+      url=url,
+      # js_api=js_api,
+      width=width,
+      height=height,
+      resizable=resizable,
+      fullscreen=fullscreen,
+      min_size=min_size,
+      strings=strings,
+      confirm_quit=confirm_quit,
+      background_color=background_color,
+      debug=debug,
+      text_select=text_select
     )
 
     return self
