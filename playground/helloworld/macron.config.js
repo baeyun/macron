49cbea52f2daf7e68f172d14af2336e58f2ec97a
Bukhari M <bukharim96@gmail.com>
2018-08-19 21:54:25 +0200

    Deprecate pywebview

--- playground/helloworld/macron.config.js
+++ playground/helloworld/macron.config.js
@@ -1,13 +1,34 @@
 /**
-* Macron Configuration File
-*/
+ * Macron Configuration File
+ */
+const { Window } = require('macron')
+
+const MainView = new Window({
+  title: 'Hello World App',
+  sourcePath: './public/index.html',
+  nativeDependencies: ['numpy.py']
+})
+
+const AuthView = new Window({
+  title: 'Login',
+  sourcePath: './public/login.html',
+  nativeDependencies: [
+    'auth.py',
+    'oauth.py',
+    'fbauthsdk.py',
+    'Microsoft.x32.NET_SDK.dll'
+  ]
+})
+
+// AuthView.on('close', function() {
+//   console.log('AuthView closed')
+// })
 
 module.exports = {
-  name: 'Hello World App | ש',
+  mainWindow: IsLoggedIn ? MainView : AuthView,
   devServerURI: 'https://mail.google.com',
-  sourcePath: './public/index.html',
-  // nativeScriptsPath: './native',
-  // logoPath: './src/img/logo.png',
-  // autoGenerateLogoSizes: true,
+  nativeScriptsPath: './native/',
+  logoPath: './src/img/logo.png',
+  autoGenerateLogoSizes: true,
   buildPath: './builds'
 }
