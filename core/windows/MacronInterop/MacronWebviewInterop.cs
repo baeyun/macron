6e59280dfee4578e1c32ff5634e52e1a94521123
Bukhari M <bukharim96@gmail.com>
2018-08-26 08:52:47 +0200

    Rebuild assemblies

--- core/windows/MacronInterop/MacronWebviewInterop.cs
+++ core/windows/MacronInterop/MacronWebviewInterop.cs
@@ -9,9 +9,23 @@ namespace MacronWebviewInterop
     {
         object eval_python(string script);
 
+        object call_common_module_classmethod(
+            string module_name,
+            string class_name,
+            string method_name,
+            object args
+        );
+
         object call_module_func(
             string module_name,
             string func_name,
+            object args
+        );
+
+        object call_module_classproperty(
+            string module_name,
+            string class_name,
+            string property_name,
             object args
         );
 
