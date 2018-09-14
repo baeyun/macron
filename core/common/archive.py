0d550329bd8c01f82745ca2a8403cbf06217e764
Bukhari M <bukharim96@gmail.com>
2018-09-14 02:34:56 +0200

    Update Common APIs to latest Bridge standards

--- core/common/archive.py
+++ core/common/archive.py
@@ -5,12 +5,24 @@ For encrypted archives (encrypted bytes, not regular passwords), check the
 crypto module.
 """
 
-from macron import NativeBridge
+from macron import *
 
-from zipfile import ZipFile, ZIP64_LIMIT, ZIP64_VERSION, ZIP_FILECOUNT_LIMIT, ZIP_FILECOUNT_LIMIT, ZIP_BZIP2, ZIP_DEFLATED, ZIP_LZMA, ZIP_MAX_COMMENT, ZIP_STORED
+from zipfile import (
+  ZipFile,
+  ZIP64_LIMIT,
+  ZIP64_VERSION,
+  ZIP_FILECOUNT_LIMIT,
+  ZIP_FILECOUNT_LIMIT,
+  ZIP_BZIP2,
+  ZIP_DEFLATED,
+  ZIP_LZMA,
+  ZIP_MAX_COMMENT,
+  ZIP_STORED
+)
 
 class Archive(NativeBridge):
   # @todo Resolve relative paths
+  @macronMethod
   def write_zip(self, zipname, files):
     with ZipFile(zipname, 'w') as zipf:
       for f in files:
@@ -18,6 +30,7 @@ class Archive(NativeBridge):
         zipf.write(f)
   
   # @todo Enhance compatibility with HTML (UTF-8)
+  @macronMethod
   def read_zip(self, zipname, file):
     with ZipFile(zipname) as zipf:
       with zipf.open(file) as f:
@@ -25,27 +38,33 @@ class Archive(NativeBridge):
   
   # Warning: This should not be available for disclosure
   # @todo Convert str to bytes for pwd
+  @macronMethod
   def set_password(self, zipname, pwd=None):
     ZipFile(zipname).setpassword(bytes(pwd))
     return
   
+  @macronMethod
   def extract_member(self, zipname, member=None, extraction_path="/"):
     ZipFile(zipname).extract(member, path=extraction_path)
     return
   
+  @macronMethod
   def extract_all(self, zipname, extraction_path="/"):
     ZipFile(zipname).extractall(extraction_path)
     return
   
   # @todo Convert ZipInfo class to JavaScript object
+  @macronMethod
   def getinfo(self, zipname, key):
     # return ZipFile(zipname).getinfo(key)
     # Return ZIP name for now
     return ZipFile(zipname).filename
   
   # @todo Get zip size
+  @macronMethod
   def get_size(self, zipname):
     return ZipFile(zipname).file_size
   
+  @macronMethod
   def get_children(self, zipname):
     return ZipFile(zipname).namelist().__str__()
--- core/common/system.py
+++ core/common/system.py
@@ -1,42 +1,53 @@
-from macron import NativeBridge
+from macron import *
 
 import platform
 
 class System(NativeBridge):
+  @macronMethod
   def platform(self):
     return platform.platform()
 
+  @macronMethod
   def system(self):
     return platform.version()
   
+  @macronMethod
   def machine(self):
     return platform.machine()
     
+  @macronMethod
   def node(self):
     return platform.node()
   
+  @macronMethod
   def processor(self):
     return platform.processor()
   
+  @macronMethod
   def release(self):
     return platform.release()
   
+  # @macronMethod
   # def system_alias(self):
   #   return platform.system_alias()
   
+  @macronMethod
   def uname(self):
     return str(platform.uname()) # @expect to return JavaScript array
   
   # Java Platform
+  @macronMethod
   def java_ver(self):
     return str(platform.java_ver()) # @expect to return JavaScript array
   
   # Win32 Platform
+  @macronMethod
   def win32_ver(self):
     return str(platform.win32_ver()) # @expect to return JavaScript array
   
   # Win95/98 Platform. Deprecated since version 3.3
   # We are downgrading to version 2.7 anyways.
+  # @macronMethod
   # def popen(self):
   #   return platform.popen()
   
@@ -47,13 +58,16 @@ class System(NativeBridge):
   #         // Run MacOS-specific commands
   #       }
   #     }
+  @macronMethod
   def mac_ver(self):
     return str(platform.mac_ver()) # 
   
   # Unix Platforms
   # Deprecated since version 3.5, will be removed in version 3.7
+  @macronMethod
   def dist(self):
     return str(platform.dist()) # Alias for linux_distribution. @expect to return JavaScript array
   
+  @macronMethod
   def libc_ver(self):
     return str(platform.libc_ver()) # @expect to return JavaScript array
